5455f379e0d547e275c10252c8326cd3f87f3d80
arapoport918 <arapoport918@gmail.com>
2025-01-25 20:21:57 -0500

    Basic stats table implementation and design

--- src/stats-table/stats-table.component.ts
+++ src/stats-table/stats-table.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-stats-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './stats-table.component.html',
   styleUrl: './stats-table.component.scss'
 })
 export class StatsTableComponent {
-
+  headers: string[] = [
+    'Name', 'Year', 'Team', 'Position', 'BA', 'RBI', 'OBP', 'SLG', 'OPS', 'RC', 'ISO', 'wOBA', 'wRC+', 'OPS+', 'BsR'
+  ];
 }
